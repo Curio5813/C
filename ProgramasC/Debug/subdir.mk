4b6317da8a74f03be6f04914576ad6200ecede7f
Curio5813 <marcosglasner@gmail.com>
2023-10-25 15:40:20 +0100

    Adding programs to the repository.

--- ProgramasC/Debug/subdir.mk
+++ ProgramasC/Debug/subdir.mk
@@ -4,13 +4,13 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../exercicio26.c 
+../exercicio05.c 
 
 OBJS += \
-./exercicio26.o 
+./exercicio05.o 
 
 C_DEPS += \
-./exercicio26.d 
+./exercicio05.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
